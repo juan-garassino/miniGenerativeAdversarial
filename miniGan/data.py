bcd1fbabaca2a8faf33a910624af6b18039a3cf2
juan garassino <juan.garassino@hotmail.com>
2022-09-06 14:59:43 +0200

    enough for now

--- miniGan/data.py
+++ miniGan/data.py
@@ -8,3 +8,7 @@ def mnist_data():
     )  # ((.5, .5, .5), (.5, .5, .5))
     out_dir = "./miniGan/data"
     return datasets.MNIST(root=out_dir, train=True, transform=compose, download=True)
+
+
+def get_custom_data():
+    pass
