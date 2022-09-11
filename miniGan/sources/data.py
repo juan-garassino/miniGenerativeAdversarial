066e2f5869d31d51decf222db334014da694c3b8
juan garassino <juan.garassino@hotmail.com>
2022-09-11 19:24:55 +0200

    formated and running in colab

--- miniGan/sources/data.py
+++ miniGan/sources/data.py
@@ -1,5 +1,6 @@
 from torchvision import transforms
 from torchvision import datasets
+import os
 
 
 def mnist_data():
@@ -7,7 +8,14 @@ def mnist_data():
         [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
     )  # ((.5, .5, .5), (.5, .5, .5))
     out_dir = "./miniGan/data"
-    return datasets.MNIST(root=out_dir, train=True, transform=compose, download=True)
+
+    if os.environ.get('DATASET') == 'mnist':
+        return datasets.MNIST(root=out_dir, train=True, transform=compose, download=True)
+
+    return datasets.FashionMNIST(root=out_dir,
+                                 train=True,
+                                 transform=compose,
+                                 download=True)
 
 
 def get_custom_data():
